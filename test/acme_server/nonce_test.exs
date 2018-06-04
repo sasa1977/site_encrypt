8bab1126e991ba26bae9392f9d6c140f3273963b
Andrew Forward <aforward@gmail.com>
2018-06-04 09:20:41 -0400

    Add property assertions to Nonce generator

--- test/acme_server/nonce_test.exs
+++ test/acme_server/nonce_test.exs
@@ -1,5 +1,6 @@
 defmodule AcmeServer.NonceTest do
   use ExUnit.Case, async: true
+  use ExUnitProperties
 
   test "new is unique" do
     first_nonce = AcmeServer.Nonce.new()
@@ -21,9 +22,40 @@ defmodule AcmeServer.NonceTest do
     end
   end
 
-  test "verify unknown nonce throw error" do
+  test "verify! unknown nonce throw error" do
     assert_raise MatchError, fn ->
       AcmeServer.Nonce.verify!(-1)
+    end
+  end
+
+  # TODO: Look at adding StreamData.repeatedly to StreamData
+  # This code is based on https://elixirforum.com/t/how-to-create-a-custom-streamdata-generator/11935/9
+  defp nonce() do
+    :ignore
+    |> StreamData.constant()
+    |> StreamData.bind(fn _ -> StreamData.constant(AcmeServer.Nonce.new()) end)
+  end
+
+  property "nonce is always unique" do
+    check all nonce_a <- nonce(),
+              nonce_b <- nonce() do
+      assert nonce_a != nonce_b
+    end
+  end
+
+  property "nonce is always verifiable" do
+    check all nonce_a <- nonce() do
+      assert :ok == AcmeServer.Nonce.verify!(nonce_a)
+    end
+  end
+
+  property "nonce is can only be verified once" do
+    check all nonce_a <- nonce() do
+      :ok = AcmeServer.Nonce.verify!(nonce_a)
+
+      assert_raise MatchError, fn ->
+        AcmeServer.Nonce.verify!(nonce_a)
+      end
     end
   end
 end
