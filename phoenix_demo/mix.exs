1e9270480bbbe0dfaa38ea211be3ea73f27ccab6
Sasa Juric <sasa.juric@gmail.com>
2018-07-11 11:17:30 +0200

    use local dependency in a demo project

--- phoenix_demo/mix.exs
+++ phoenix_demo/mix.exs
@@ -38,7 +38,7 @@ defmodule PhoenixDemo.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:site_encrypt, github: "sasa1977/site_encrypt"}
+      {:site_encrypt, path: ".."}
     ]
   end
 end
