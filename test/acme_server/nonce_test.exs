399c98fa7d18400426534dc109671c9dce47b256
Saša Jurić <sasa.juric@gmail.com>
2020-05-03 15:39:24 +0200

    Fix formatting

--- test/acme_server/nonce_test.exs
+++ test/acme_server/nonce_test.exs
@@ -29,13 +29,13 @@ defmodule AcmeServer.NonceTest do
   end
 
   property "nonce is always unique", context do
-    check all nonces <- nonempty(list_of(nonce(context.config))) do
+    check all(nonces <- nonempty(list_of(nonce(context.config)))) do
       assert Enum.uniq(nonces) == nonces
     end
   end
 
   property "nonce is always verifiable", context do
-    check all nonce <- nonce(context.config) do
+    check all(nonce <- nonce(context.config)) do
       assert :ok == AcmeServer.Nonce.verify!(context.config, nonce)
     end
   end
@@ -43,13 +43,13 @@ defmodule AcmeServer.NonceTest do
   property "valid nonce is not verifiable in another server", context do
     another_server_config = start_acme_server()
 
-    check all nonce <- nonce(context.config) do
+    check all(nonce <- nonce(context.config)) do
       assert_raise MatchError, fn -> AcmeServer.Nonce.verify!(another_server_config, nonce) end
     end
   end
 
   property "nonce can only be verified once", context do
-    check all nonce <- nonce(context.config) do
+    check all(nonce <- nonce(context.config)) do
       :ok = AcmeServer.Nonce.verify!(context.config, nonce)
       assert_raise MatchError, fn -> AcmeServer.Nonce.verify!(context.config, nonce) end
     end
